17319a5d32cabe4d382aff5d752a9d32768632db
Advik <advik.b@gmail.com>
2024-12-12 10:50:50 +0000

    Add filesystem support to main.cpp for temporary directory handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <sstream>
 #include <elzip.hpp>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 std::string decodeKey(const std::string& encodedKey) {
     std::string key;
@@ -14,6 +17,16 @@ std::string decodeKey(const std::string& encodedKey) {
 
 int main() {
     std::cout <<  decodeKey(CURSEFORGE_API_KEY) << "\n";   
+    // elz::extractFile("test.zip", "test.txt");
+    try {
+        // Get the path to the temporary directory
+        fs::path tempPath = fs::temp_directory_path();
+        std::cout << tempPath / "mcmpdl";
+        
+        std::cout << "Temporary directory: " << tempPath << std::endl;
+    } catch (const fs::filesystem_error& e) {
+        std::cerr << "Error getting temp directory: " << e.what() << std::endl;
+    }
     return 0;
 }
 
