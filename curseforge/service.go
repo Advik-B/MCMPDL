b5a9755397fd1d3c4e4f5944778f30e9374cbd9d
Advik <advik.b@gmail.com>
2025-03-19 01:11:34 +0530

    Add service methods for fetching game versions and categories

--- curseforge/service.go
+++ curseforge/service.go
@@ -45,3 +45,57 @@ func (s *Service) GetGame(gameId int) (schemas.Game, error) {
 	}
 	return gameResponse.Data, nil
 }
+
+func (s *Service) GetVersions(gameId int) (schemas.GetVersionsResponseV1, error) {
+	versionsResponse := schemas.GetVersionsResponseV1{}
+	res, err := s.api.Fetch("/v1/games/" + strconv.Itoa(gameId) + "/versions")
+	if err != nil {
+		return versionsResponse, err
+	}
+	err = json.Unmarshal(res, &versionsResponse)
+	if err != nil {
+		return versionsResponse, err
+	}
+	return versionsResponse, nil
+}
+
+func (s *Service) GetVersionTypes(gameId int) (schemas.GetVersionTypesResponse, error) {
+	versionTypesResponse := schemas.GetVersionTypesResponse{}
+	res, err := s.api.Fetch("/v1/games/" + strconv.Itoa(gameId) + "/version-types")
+	if err != nil {
+		return versionTypesResponse, err
+	}
+	err = json.Unmarshal(res, &versionTypesResponse)
+	if err != nil {
+		return versionTypesResponse, err
+	}
+	return versionTypesResponse, nil
+}
+
+func (s *Service) GetVersionsV2(gameId int) (schemas.GetVersionsResponseV2, error) {
+	versionsResponse := schemas.GetVersionsResponseV2{}
+	res, err := s.api.Fetch("/v2/games/" + strconv.Itoa(gameId) + "/versions")
+	if err != nil {
+		return versionsResponse, err
+	}
+	err = json.Unmarshal(res, &versionsResponse)
+	if err != nil {
+		return versionsResponse, err
+	}
+	return versionsResponse, nil
+}
+
+func (s *Service) GetCategories() ([]schemas.Category, error) {
+	var categoriesResponse struct {
+		Data []schemas.Category `json:"data"`
+	}
+	res, err := s.api.Fetch("/v1/categories")
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(res, &categoriesResponse)
+	if err != nil {
+		return nil, err
+	}
+	return categoriesResponse.Data, nil
+}
